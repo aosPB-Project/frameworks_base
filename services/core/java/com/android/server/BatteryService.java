5841aa69a3a556723df45bcbbfb4aabc57c93403
Jyotiraditya Panda <jyotiraditya@aospa.co>
2024-02-22 23:25:05 +0900

    core: BatteryManager: Add few more Battery broadcast intents
     * maximum_capacity
     * design_capacity
    
    Change-Id: Idfa9207cc8bd35469876f3094f3bc948741c94bf
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -267,6 +267,8 @@ public final class BatteryService extends SystemService {
      * {@link android.Manifest.permission#BATTERY_STATS} permission and should therefore not be
      * included in the ACTION_BATTERY_CHANGED intent extras.
      */
+    private int mLastBatteryFullCharge;
+    private int mLastBatteryFullChargeDesign;
     private int mLastChargingPolicy;
 
     private int mSequence = 1;
@@ -766,6 +768,8 @@ public final class BatteryService extends SystemService {
                 || mHealthInfo.maxChargingVoltageMicrovolts != mLastBroadcastMaxChargingVoltage
                 || includeChargeCounter
                 || mInvalidCharger != mLastBroadcastInvalidCharger
+                || mHealthInfo.batteryFullChargeUah != mLastBatteryFullCharge
+                || mHealthInfo.batteryFullChargeDesignCapacityUah != mLastBatteryFullChargeDesign
                 || mHealthInfo.batteryCycleCount != mLastBroadcastBatteryCycleCount
                 || mHealthInfo.chargingState != mLastBroadcastChargingState
                 || mHealthInfo.batteryCapacityLevel != mLastBroadcastBatteryCapacityLevel)) {
@@ -998,6 +1002,8 @@ public final class BatteryService extends SystemService {
                 mLastBroadcastBatteryCycleCount = mHealthInfo.batteryCycleCount;
                 mLastBroadcastChargingState = mHealthInfo.chargingState;
                 mLastBroadcastBatteryCapacityLevel = mHealthInfo.batteryCapacityLevel;
+                mLastBatteryFullCharge = mHealthInfo.batteryFullChargeUah;
+                mLastBatteryFullChargeDesign = mHealthInfo.batteryFullChargeDesignCapacityUah;
             }
         }
     }
@@ -1033,6 +1039,8 @@ public final class BatteryService extends SystemService {
         intent.putExtra(BatteryManager.EXTRA_CYCLE_COUNT, mHealthInfo.batteryCycleCount);
         intent.putExtra(BatteryManager.EXTRA_CHARGING_STATUS, mHealthInfo.chargingState);
         intent.putExtra(BatteryManager.EXTRA_CAPACITY_LEVEL, mHealthInfo.batteryCapacityLevel);
+        intent.putExtra(BatteryManager.EXTRA_MAXIMUM_CAPACITY, mHealthInfo.batteryFullChargeUah);
+        intent.putExtra(BatteryManager.EXTRA_DESIGN_CAPACITY, mHealthInfo.batteryFullChargeDesignCapacityUah);
         if (DEBUG) {
             Slog.d(TAG, "Sending ACTION_BATTERY_CHANGED. scale:" + BATTERY_SCALE
                     + ", info:" + mHealthInfo.toString());
@@ -1136,6 +1144,8 @@ public final class BatteryService extends SystemService {
         event.putInt(BatteryManager.EXTRA_CYCLE_COUNT, mHealthInfo.batteryCycleCount);
         event.putInt(BatteryManager.EXTRA_CHARGING_STATUS, mHealthInfo.chargingState);
         event.putInt(BatteryManager.EXTRA_CAPACITY_LEVEL, mHealthInfo.batteryCapacityLevel);
+        event.putInt(BatteryManager.EXTRA_MAXIMUM_CAPACITY, mHealthInfo.batteryFullChargeUah);
+        event.putInt(BatteryManager.EXTRA_DESIGN_CAPACITY, mHealthInfo.batteryFullChargeDesignCapacityUah);
 
         boolean queueWasEmpty = mBatteryLevelsEventQueue.isEmpty();
         mBatteryLevelsEventQueue.add(event);
