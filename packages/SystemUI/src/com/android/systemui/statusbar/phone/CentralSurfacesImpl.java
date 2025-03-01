7aac0798a2c804757fb093eda8a15ebc2fde1adb
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-03-01 19:34:44 +0545

    Revert "[sb] remove unused param from initializer callback"
    Required for "SystemUI: Implement burn-in protection for status/navbar"
    
    This reverts commit d40257cfd6d35ad5a398796cd17d6ba38bba0ec5.
    
    Change-Id: I234d186c69767eaa15c99130c5263002e290ca3a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -1209,7 +1209,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
 
         // Set up CollapsedStatusBarFragment and PhoneStatusBarView
         mStatusBarInitializer.setStatusBarViewUpdatedListener(
-                (statusBarViewController, statusBarTransitions) -> {
+                (statusBarView, statusBarViewController, statusBarTransitions) -> {
+                    mStatusBarView = statusBarView;
                     mPhoneStatusBarViewController = statusBarViewController;
                     mStatusBarTransitions = statusBarTransitions;
                     getNotificationShadeWindowViewController()
